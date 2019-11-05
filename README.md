4c725e7faf75dd0d08b6e3e503db35ceafa9c47c
Graeme Stewart <gi_stewart@yahoo.com>
2019-11-04 23:19:28 -0500

    adds time telling code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Train-Scheduler
+
+## Project Functionality
+This is a game for web browsers where the user answers a number of questions.  Radio buttons represent 4 possible answers to each question. The radio buttons mean only 1 answer can be provided. As soon as the game starts, a timer begins, and when the timer expires, the game ends and the results are shown. The user can opt to see the results earlier by clicking a Done button. Results for correct answers, incorrect answers and unanswered questions are shown.
+
+## Project Usefulness
+This project demonstrates the applicaton of the jQuery library including click events, a timer, content being hidden and displayed, and updates to the HTML file based on user behavior. 
+
+## How to get started
+On page load, the user just needs to click Start to start the game. The user can play as many times as they wish by refreshing the page, but the questions remain the same. 
+
+## How to get help
+[jQuery Official Website](https://jquery.com/)
+
+## Project maintenance and contributions
+This is not an original game.  Instead, this project was prepared as part of an assignment for Georgia Tech's Coding Boot Camp.
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,121 @@
 $(document).ready(function() {
 
-    //declare global variables for ...
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyCNQyqD5E_ONJbubmNqtrhqGeCTH_DAWtc",
+        authDomain: "my-first-firebase-projec-b48bb.firebaseapp.com",
+        databaseURL: "https://my-first-firebase-projec-b48bb.firebaseio.com",
+        projectId: "my-first-firebase-projec-b48bb",
+        storageBucket: "my-first-firebase-projec-b48bb.appspot.com",
+        messagingSenderId: "64082316541",
+        appId: "1:64082316541:web:186428a5c9e8188b554294"
+    };
 
-});
+    firebase.initializeApp(firebaseConfig);
+
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    // Capture Button Click
+    $("#add-train").on("click", function(event) {
+        // prevent page from refreshing when form tries to submit itself
+        event.preventDefault();
+
+        // Capture user inputs and store them into variables
+        var trainName = $("#name-input").val().trim();
+        var trainDestination = $("#destination-input").val().trim();
+        var trainFirstTime = $("#first-train-input").val().trim();
+        var trainFrequency = $("#frequency-input").val().trim();
+
+        // Console log each of the user inputs
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainFirstTime);
+        console.log(trainFrequency);
+
+        //declare local temp object for holding employee data
+        var newTrain = {
+            name: trainName,
+            destination: trainDestination,
+            firstTime: trainFirstTime,
+            frequency: trainFrequency
+        };
+
+        //upload object to db
+        database.ref().push(newTrain);
+
+        //log to console
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.firstTime);
+        console.log(newTrain.frequency);
+
+        //clear text boxes
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#first-train-input").val("");
+        $("#frequency-input").val("");
+
+    });
+
+    //Create Firebase event for adding an employee to the db and the changes to the DOM
+    database.ref().on("child_added", function(trainSnapshot) {
+        console.log(trainSnapshot.val());
+
+        // Store everything into a variable.
+        var trainName = trainSnapshot.val().name;
+        var trainDestination = trainSnapshot.val().destination;
+        var trainFirstTime = trainSnapshot.val().firstTime;
+        var trainFrequency = trainSnapshot.val().frequency;
+
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainFirstTime);
+        console.log(trainFrequency);
+
+        //Calculate the Next Arrival and Minutes Away values
+
+        // 1. First time (t minus 1 year)
+        var trainFirstTimeConverted = "";
+        trainFirstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+        console.log(trainFirstTimeConverted);
+
+        // 2. Current time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // 3. Difference between the times
+        var diffTime = moment().diff(moment(trainFirstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // 4. Minutes since last train
+        var trainRemainder = diffTime % trainFrequency;
+        console.log(trainRemainder);
+
+        // 5. Minutes until next train
+        var trainMinutesTillTrain = trainFrequency - trainRemainder;
+        console.log("MINUTES TILL TRAIN: " + trainMinutesTillTrain);
+
+        // 6. Next Train arrival time
+        var nextTrain = moment().add(trainMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        // Create the new row
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(trainDestination),
+            $("<td>").text(trainFrequency)
+            //next arrival
+            //minutes away
+        );
+
+        //Append the new role to the table 
+        $("#train-schedule > tbody").append(newRow);
+
+    });
+
+    // function(errorObject) {
+    //     console.log("The read failed: " + errorObject.code);
+    // });
+
+})
